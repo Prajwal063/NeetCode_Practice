ee26af40e29dfa55aa493d0fc7c651dd0721e00e
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2023-10-15 00:02:27 +0530

    Time: 6 ms (79.15%), Space: 43.1 MB (68.55%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -31,39 +31,3 @@ class Solution {
         return stack.peek();
     }
 }
-
-// class Solution {
-//     public int evalRPN(String[] tokens) {
-//         Stack<Integer> stack = new Stack();
-//         int n = tokens.length;
-        
-//         for(String i : tokens){
-//             if (i.matches("-?\\d+")) {
-//                 stack.push(Integer.parseInt(i));
-//             }
-//             else {
-//                 int temp1 = stack.pop();
-//                 int temp = stack.pop();
-//                 int temp2;
-                
-//                 if(i.equals("+")){
-//                     temp2 = temp+temp1;
-//                 }
-//                 else if(i.equals("-")){
-//                     temp2 = temp-temp1;
-//                 }
-//                 else if(i.equals("*")){
-//                     temp2 = temp*temp1;
-//                 }
-//                 else if(i.equals("/")){
-//                     temp2 = temp/temp1;
-//                 }
-//                 else{
-//                     throw new IllegalArgumentException(i);
-//                 }
-//                 stack.push(temp2);
-//             }
-//         }
-//         return stack.peek();
-//     }
-// }
