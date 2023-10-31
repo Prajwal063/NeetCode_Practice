5a26edd448cbdc52f5095dff42073fa4d802cf71
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2023-10-31 22:25:10 +0530

    Time: 5 ms (96.59%), Space: 41.5 MB (72.62%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -10,12 +10,17 @@ class Solution {
         int i = 0, j = 0;
         int count = s1.length();
         while(j < s2.length()){
+            
+            //if any character in s2, has value more than one then decreament the value
+            //[1, 1, 0, 0, 0, ..., 0] => [1, 0, 0, 0, 0, ..., 0]
             if(arr[s2.charAt(j++) - 'a']-- > 0)
                 count--;
             if(count == 0)
                 return true;
             if(j - i == s1.length() && arr[s2.charAt(i++) - 'a']++ >= 0)
                 count++;
+            
+            
         }
         return false;
     }    
