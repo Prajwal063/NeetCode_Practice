2920e63b105ed4762641d1146740fe5d0ce602ef
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2023-11-02 00:39:32 +0530

    Time: 6 ms (87.35%), Space: 41.6 MB (64.40%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -3,24 +3,36 @@ class Solution {
         
        int[] arr = new int[26];
         
+        //increase the counter for each character in the string s1 i.e. substring
+        //we do c -'a' because we need the order of the characters like arr[0] = a, arr[1] = b etc
         for(char c: s1.toCharArray()){
             arr[c - 'a']++;
         }
+        
+        //Now arr = [1, 1, 0, 0, 0, ..., 0]
         
         int i = 0, j = 0;
         int count = s1.length();
         while(j < s2.length()){
             
             //if any character in s2, has value more than one then decreament the value
-            //[1, 1, 0, 0, 0, ..., 0] => [1, 0, 0, 0, 0, ..., 0]
+            //at j = 3 ==> [1, 1, 0, 0, 0, ..., 0] => [1, 0, 0, 0, 0, ..., 0]
+            //so count = 2-- = 1;
+            
+             //if any character in s2, has value more than one then decreament the value
+            //at j = 4 ==> [1, 0, 0, 0, 0, ..., 0] => [0, 0, 0, 0, 0, ..., 0]
+            //so count = 1-- = 0;
+            
             if(arr[s2.charAt(j++) - 'a']-- > 0)
                 count--;
+            
+            //now count = 0 so all the characters from String s1 is present in String s2 => so return true
             if(count == 0)
                 return true;
+            
+            
             if(j - i == s1.length() && arr[s2.charAt(i++) - 'a']++ >= 0)
                 count++;
-            
-            
         }
         return false;
     }    
