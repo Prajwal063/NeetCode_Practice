22cec26c647cd86426d4d3d3f5a60c8784e4e894
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2023-11-20 21:55:50 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (40.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -27,3 +27,28 @@ class Solution {
         return start.next;
     }
 }
+
+
+//LOGIC-
+//1>we keep two pointer slow and fast(both move one at a time)both initially at start of list(at the dummy node)
+//2>we move the fast to n+1 places away from the slow pointer
+//3>we then traverse the list we check if fast is equal to null or not,if it is null we know that the slow pointer      has reached just one node before the node we need to delete!
+//4>then we slow.next=slow.next.next!
+
+
+
+//suppose:
+//1->2->3->4->5->6
+//n=2
+//make a dummy node with val=0;(we call this start)
+//so now our list looks like
+//0->1->2->3->4->5->6
+//slow ,start , fast all are pointing to 0 valued node!
+//after executing step 2 of our algorithm we have
+//slow and start still at 0
+//but fast is at node with val 3;
+//now we execute step 3
+//different positions of slow and fast is shown below!
+//[slow=1,fast=4]->[slow=2,fast=5]->[slow=3,fast=6]->[slow=4,fast=null]
+//wow!!slow have reached one node before out target node
+//now just do slow.next=slow.next.next;
