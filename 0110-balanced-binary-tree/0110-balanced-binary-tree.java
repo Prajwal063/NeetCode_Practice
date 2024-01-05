3cb78a813f66de8bf765bee73a3c09986cd2902b
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-01-05 11:06:42 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (13.35%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -20,18 +20,20 @@ class Solution {
     }
     
     private int height(TreeNode root){
-        if(root == null){
+        
+        if(root == null){                                   //base condition
             return 0;
         }
-        int left = height(root.left);
-        int right = height(root.right);
         
-        if(left == -1 || right == -1){
+        int left = height(root.left);                       //calling left child node using recursion
+        int right = height(root.right);                     //calling left child node using recursion
+        
+        if(left == -1 || right == -1){                      //if there is NO left or right node, return -1
             return -1;
         }
-        if(left - right < -1 || left - right > 1){
+        if(left - right < -1 || left - right > 1){          //if diff btw left and right node <-1 or >1, return -1
             return -1;
         }
-        return Math.max(left, right) + 1;
+        return Math.max(left, right) + 1;                   //else return height
     }
 }
