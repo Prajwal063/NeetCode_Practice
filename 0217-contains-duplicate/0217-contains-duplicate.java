db12a2177b5c97acc880830671c36acfdf9703d4
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-03-05 08:53:00 +0530

    Time: 10 ms (87.59%), Space: 60.7 MB (16.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> h = new HashSet<>();
-        for(int i : nums){
+        for(int i: nums){
             if(h.contains(i))
                 return true;
             h.add(i);
