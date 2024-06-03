cd205c35070c0843bde32f7113ceea36c7ab0d07
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-06-03 22:36:25 +0530

    Time: 1 ms (98.85%), Space: 40.9 MB (63.31%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,10 +1,23 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        Arrays.sort(stones);
-        for(int i = stones.length - 1; i > 0; i--){
-            stones[i - 1] = stones[i] - stones[i - 1];
-            Arrays.sort(stones);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for(int i: stones)  
+            pq.offer(-i);
+        while(pq.size() > 1){
+            int i1 = pq.remove();
+            int i2 = pq.remove();
+            if(i1 != i2)
+                pq.add(i1 - i2);
         }
-        return stones[0];
+        return pq.size() != 0 ? (-pq.remove()) : 0;
+        
+        
+        // Arrays.sort(stones);
+        // for(int i = stones.length - 1; i > 0; i--){
+        //     stones[i - 1] = stones[i] - stones[i - 1];
+        //     Arrays.sort(stones);
+        // }
+        // return stones[0];
     }
 }
