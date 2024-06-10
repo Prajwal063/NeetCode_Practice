6ba967fc35db7889efeacfed63fec8ffb76508d2
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-06-10 08:43:53 +0530

    Time: 2 ms (79.70%), Space: 45.2 MB (86.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -8,6 +8,7 @@ class Solution {
             if(i > farthest){
                 return false;
             }
+            
             farthest = Math.max(farthest, i + nums[i]);
 
             if(farthest >= nums.length-1){
