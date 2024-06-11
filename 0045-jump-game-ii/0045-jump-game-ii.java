1617facd166f997f8ca6ee88d211389a44af2064
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-06-11 08:59:44 +0530

    Time: 2 ms (71.77%), Space: 44.9 MB (56.82%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length, i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
+	    while(lastJumpedPos < n - 1) {  
+		    maxReachable = Math.max(maxReachable, i + nums[i]);  
+		    if(i == lastJumpedPos) {			  
+			    lastJumpedPos = maxReachable;     
+			    jumps++;                          
+		    }            
+		    i++;
+	    }
+	    return jumps;   
+    }
+}
