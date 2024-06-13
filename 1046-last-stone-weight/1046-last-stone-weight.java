93e7fe9e270ab02192d9afd2d8f71027b5012681
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-06-13 07:56:54 +0530

    Time: 1 ms (98.81%), Space: 41.4 MB (11.01%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,15 +1,20 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         
+        //-i: Negating the stone values turns the min-heap behavior of the priority queue into a max-heap behavior.
+        //-pq.remove(): The final value is negated again to convert it back to the original value.
+        
         PriorityQueue<Integer> pq = new PriorityQueue();
         for(int i: stones)  
-            pq.offer(-i);
+            pq.offer(-i);       //-i is added -> smallest element will become largest element when negated: max heap
+        
         while(pq.size() > 1){
             int i1 = pq.remove();
             int i2 = pq.remove();
             if(i1 != i2)
                 pq.add(i1 - i2);
         }
+        
         return pq.size() != 0 ? (-pq.remove()) : 0;
         
         
