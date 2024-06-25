278fea7e8e04985cc0920c596086a9e89a56663e
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-06-25 09:15:02 +0530

    Time: 7 ms (93.77%), Space: 44.5 MB (95.32%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,6 +1,10 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1, right = 1000000000;
+        int left = 1, right = 1;
+        for(int pile: piles){
+            right = Math.max(pile, right);
+        }
+        
         while (left < right) {
             int mid = (left + right) / 2;
             int total = 0;
