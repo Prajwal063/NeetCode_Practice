a1d133eee3f82df7f0b2891dc23e4fe1ecf24d42
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-07-04 08:08:17 +0530

    Time: 15 ms (16.79%), Space: 44.6 MB (31.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -6,6 +6,11 @@ class Solution {
             for(int j = 0; j<9; j++){
                 char cur = board[i][j];
                 if(cur!='.'){
+                    
+                    //used strings while adding so that it's easy to know where the element is repeating
+                    //if cannot add a value in a specific row OR column OR subbox -> return false
+                    //else true
+                    
                     if(!set.add(cur + "row "+i)|| !set.add(cur +"col "+j) || 
                     !set.add(cur + "box " +i/3+ "-"+j/3 )){
                         return false;
