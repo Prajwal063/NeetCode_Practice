16d48e83d760bddcc4666bf392b56834dcb32cda
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-07-05 07:15:54 +0530

    Time: 30 ms (70.26%), Space: 51.2 MB (81.18%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
         Arrays.sort(nums);
-        LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
 
         for (int i = 0; i < nums.length - 2; i++) {
-            //Only consider non-duplicate elements for i
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int target = 0 - nums[i];
-                int left = i + 1;
-                int right = nums.length - 1;
+            
+            //if duplicates, then we've processed the triplets of that element, thus skip
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-                while (left < right) {
-                    if (nums[left] + nums[right] == target) {
-                        ArrayList<Integer> miniSol = new ArrayList<>();
-                        miniSol.add(nums[i]);
-                        miniSol.add(nums[left]);
-                        miniSol.add(nums[right]);
-                        sol.add(miniSol);
-                        while (left < right && nums[left] == nums[left + 1]) {
-                            left++;
-                        }
-                        while (left < right && nums[right] == nums[right - 1]) {
-                            right--;
-                        }
-                        left++;
-                        right--;
-                    } else if (nums[left] + nums[right] > target) {
-                        right--;
-                    } else {
-                        left++;
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
                     }
+
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
-
-        return sol;
+        return ans;
     }
 }
