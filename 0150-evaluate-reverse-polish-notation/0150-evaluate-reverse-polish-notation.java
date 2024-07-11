98b604be5da3cf79dac4ddb28564c0c6698fee90
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-07-11 08:36:29 +0530

    Time: 6 ms (70.32%), Space: 45 MB (9.23%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -7,22 +7,22 @@ class Solution {
             if(i.equals("+")){
                 int temp = stack.pop();
                 int temp1 = stack.pop();
-                stack.push(temp+temp1);
+                stack.push(temp + temp1);
             }
             else if(i.equals("-")){
                 int temp = stack.pop();
                 int temp1 = stack.pop();
-                stack.push(temp1-temp);
+                stack.push(temp1 - temp);
             }
             else if(i.equals("*")){
                 int temp = stack.pop();
                 int temp1 = stack.pop();
-                stack.push(temp*temp1);
+                stack.push(temp * temp1);
             }
             else if(i.equals("/")){
                 int temp = stack.pop();
                 int temp1 = stack.pop();
-                stack.push(temp1/temp);
+                stack.push(temp1 / temp);
             }
             else{
                 stack.push(Integer.parseInt(i));
