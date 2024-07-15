8bd496f0e69b7def74397c0908b5914aa1bdc71f
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-07-15 07:48:27 +0530

    Time: 22 ms (83.13%), Space: 47.8 MB (94.13%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        //We can completely ignore the square root as we are just comparing the values (if a*a>b*b => a>b)
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) ->
+            Integer.compare(
+                (a[0] * a[0] + a[1] * a[1]),
+                (b[0] * b[0] + b[1] * b[1])
+            )
+        );
+        for (int[] point : points) {
+            q.add(point);
+        }
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            int[] cur = q.poll();
+            ans[i][0] = cur[0];
+            ans[i][1] = cur[1];
+        }
+        return ans;
+    }
+}
