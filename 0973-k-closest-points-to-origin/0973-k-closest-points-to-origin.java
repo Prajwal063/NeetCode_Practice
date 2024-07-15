70c3a5b6b390230a7f3fe10fdfdfad066f36239f
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-07-15 07:49:11 +0530

    Time: 23 ms (77.27%), Space: 47.7 MB (95.16%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -3,16 +3,12 @@ class Solution {
         //We can completely ignore the square root as we are just comparing the values (if a*a>b*b => a>b)
         PriorityQueue<int[]> q = new PriorityQueue<>((a, b) ->
             Integer.compare(
-                (b[0] * b[0] + b[1] * b[1]),
-                (a[0] * a[0] + a[1] * a[1])
+                (a[0] * a[0] + a[1] * a[1]),
+                (b[0] * b[0] + b[1] * b[1])
             )
-        ); //only this is changed (swapped)
+        );
         for (int[] point : points) {
             q.add(point);
-            //remove when size increase k
-            if (q.size() > k) {
-                q.remove();
-            }
         }
         int[][] ans = new int[k][2];
         for (int i = 0; i < k; i++) {
