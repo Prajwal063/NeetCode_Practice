7952487cbffd0977134a0edf2f207f267dd03d0f
Prajwal P <85285208+Prajwal063@users.noreply.github.com>
2024-07-31 08:47:29 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (22.76%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -13,13 +13,12 @@ class Solution {
         ListNode cur = head;
         ListNode prev = null;
         ListNode next = null;
-        
         while(cur!=null){
             next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = next;
-        }        
+        }
         return prev;
     }
 }
